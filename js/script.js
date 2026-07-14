cce368c343c02a34970fd5aed9ec8e3990536561
pushpakumarihazampur1-ops <pushpakumari.hazampur1@gmail.com>
2026-07-14 17:02:17 +0530

    Add dark mode toggle functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+const darkBtn = document.getElementById("darkBtn");
 
+darkBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        darkBtn.innerHTML = "☀️";
+    }else{
+        darkBtn.innerHTML = "🌙";
+    }
+});
